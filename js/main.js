bb70dca68862c45d62a97bada586d94acd5012f5
Brandon Lobo <brandon_slobo@hotmail.com>
2022-06-01 13:43:35 -0500

    added fetch api function to main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 
-document.querySelector('').addEventListener('click',getRappername);
+document.querySelector('button').addEventListener('click',getRappername);
 
-function getRappername(){
+async function getRappername(){
 
-    const name = document
+    const name = document.querySelector('input').value;
+    const res = await fetch(`/api/${name}`);
+    const data = await res.json;
+
+    console.log(data);
+    document.querySelector('h2').textContent = data.birthName;
 
 }
+
+// 
+// const userName = document.querySelector("#userName").value;
+// const res = await fetch(`/api?student=${userName}`)
+// const data = await res.json()
+// 
+// console.log(data);
+// document.querySelector("#personName").textContent = data.name
+// document.querySelector("#personStatus").textContent = data.status
+// document.querySelector("#personOccupation").textContent = data.currentOccupation
